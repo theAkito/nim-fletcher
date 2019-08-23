a71aa219875e349bab95af08cb9b0744fe3dce7b
Akito <akito.kitsune@protonmail.com>
2019-08-23 21:40:49 +0200

    Fletcher procedure now accepts seqs and arrays as parameter

--- fletcher/f16.nim
+++ fletcher/f16.nim
@@ -1,35 +1,32 @@
-proc fletcher*(tdata: seq[uint8], tlength: uint): uint =
+proc fletcher*(data: openArray[uint8], length: uint64): uint =
   var
     c0: uint32 = 0
     c1: uint32 = 0
-    data = tdata
-    length = tlength
-    i = 0
+    i: uint8 = 0
     h: uint = 0
-    j = 0
-
+    j: uint64 = 0
   const
-    buf: uint = 5802
+    buf: uint64 = 5802
     
   while length >= buf:
     for i in countdown(length, 0, buf):
-      while h < buf:
+      while h < buf.uint32:
         c0 += data[h]
-        data[h].inc
+        h.inc
         c1 += c0
         h.inc
     c0 = c0 mod 255
     c1 = c1 mod 255
-
-  for j in 0..<length:
-    c0 = c0 + data[j]
-    data[j].inc
-    c1 = c1 + c0
+    
+  while j < length:
+    c0 += data[j]
+    j.inc
+    c1 += c0
 
   c0 = c0 mod 255
   c1 = c1 mod 255
 
   return c1 shl 8 or c0
 
-# echo fletcher(@[97.uint8, 98.uint8, 99.uint8, 100.uint8, 101.uint8], 5.uint) # correct
-# echo fletcher(@[97.uint8, 98.uint8, 99.uint8, 100.uint8, 101.uint8, 102.uint8], 6.uint) # correct
+#echo fletcher([97.uint8, 98.uint8, 99.uint8, 100.uint8, 101.uint8], 5.uint) # correct = 51440
+#echo fletcher([97.uint8, 98.uint8, 99.uint8, 100.uint8, 101.uint8, 102.uint8], 6.uint) # correct = 8279
--- fletcher/prep.nim
+++ fletcher/prep.nim
@@ -1,4 +1,19 @@
-import parseutils
+import parseutils, streams
 
 # Parse proc for every Fletcher level by parameter
 # Split by each 4 bit in binary sequence, then process with parseBin
+
+#proc convertBin*
+
+var file: File
+
+if open(file, "f16", fmRead, -1):
+  var strm = newFileStream(file)
+  var line = ""
+  while strm.readLine(line):
+    echo line
+  strm.close()
+
+discard file.open("f16", fmRead, -1)
+#echo readBytes(f = file, len = 99999)
+echo file.readAll()
