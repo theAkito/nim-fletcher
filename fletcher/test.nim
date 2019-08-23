45568bfcc07aa5e20a3ee1ffb01d0136bb1b1d6d
Akito <akito.kitsune@protonmail.com>
2019-08-23 23:43:59 +0200

    Corrected test descriptions

--- fletcher/test.nim
+++ fletcher/test.nim
@@ -9,8 +9,8 @@ echo "-abcdef- checksum; should be 8279 = "
 echo f16.fletcher([97.uint8, 98.uint8, 99.uint8, 100.uint8, 101.uint8, 102.uint8], 6.uint) # correct = 8279
 
 echo "FLETCHER32"
-echo "-abcde- checksum = "
+echo "-abcde- checksum; should be 4031760169 = "
 echo fletcher([25185.uint16, 25699.uint16, 101.uint16], 3.uint64) # correct = 4031760169
-echo "-abcde- checksum = "
+echo "-abcdef- checksum; should be 1448095018 = "
 echo fletcher([25185.uint16, 25699.uint16, 26213.uint16], 3.uint64) # correct = 1448095018
 
