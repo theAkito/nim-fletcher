3b77b1018cb4b816957a8f58f4264e3ed798d083
Akito <akito.kitsune@protonmail.com>
2019-08-23 22:53:16 +0200

    Fletcher64 prototype working!

---
+++ fletcher/f32.nim
@@ -0,0 +1,37 @@
+proc fletcher*(data: openArray[uint16], length: uint64): uint =
+  var
+    c0: uint32 = 0
+    c1: uint32 = 0
+    i: uint8 = 0
+    h: uint32 = 0
+    j: uint64 = 0
+  const
+    buf: uint64 = 360
+    fin: uint16 = 65535
+    
+  while length >= buf:
+    for i in countdown(length, 0, buf):
+      while h < buf.uint32:
+        c0 += data[h]
+        h.inc
+        c1 += c0
+        h.inc
+    c0 = c0 mod fin
+    c1 = c1 mod fin
+    
+  while j < length:
+    c0 += data[j]
+    j.inc
+    c1 += c0
+
+  c0 = c0 mod fin
+  c1 = c1 mod fin
+
+  return c1 shl 16 or c0
+
+#[
+echo "-abcde- checksum = "
+echo fletcher([25185.uint16, 25699.uint16, 101.uint16], 3.uint64) # correct = 4031760169
+echo "-abcde- checksum = "
+echo fletcher([25185.uint16, 25699.uint16, 26213.uint16], 3.uint64) # correct = 1448095018
+]#
