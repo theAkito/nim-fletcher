87b0d0b36e4d5c2b17dce7e53b948622d2a817b7
Akito <akito.kitsune@protonmail.com>
2019-08-24 01:10:32 +0200

    Fletcher64 prototype working!

---
+++ fletcher/f64.nim
@@ -0,0 +1,28 @@
+proc fletcher*(data: openArray[uint32], length: uint64): uint =
+  var
+    c0: uint64 = 0
+    c1: uint64 = 0
+    h: uint32 = 0
+    j: uint64 = 0
+  const
+    buf: uint64 = 360
+    fin: uint64 = 4_294_967_295'u64
+    
+  while length >= buf:
+    for i in countdown(length, 0, buf):
+      while h < buf.uint32:
+        c0 += data[h]
+        h.inc
+        c1 += c0
+    c0 = c0 mod fin
+    c1 = c1 mod fin
+    
+  while j < length:
+    c0 += data[j]
+    j.inc
+    c1 += c0
+
+  c0 = c0 mod fin
+  c1 = c1 mod fin
+
+  return c1.uint shl 32 or c0.uint
--- fletcher/test.nim
+++ fletcher/test.nim
@@ -1,16 +1,25 @@
 import
   f16,
-  f32
+  f32,
+  f64
 
 echo "FLETCHER16"
 echo "-abcde- checksum; should be 51440 = "
 echo f16.fletcher([97.uint8, 98.uint8, 99.uint8, 100.uint8, 101.uint8], 5.uint) # correct = 51440
 echo "-abcdef- checksum; should be 8279 = "
 echo f16.fletcher([97.uint8, 98.uint8, 99.uint8, 100.uint8, 101.uint8, 102.uint8], 6.uint) # correct = 8279
+echo ""
 
 echo "FLETCHER32"
 echo "-abcde- checksum; should be 4031760169 = "
 echo fletcher([25185.uint16, 25699.uint16, 101.uint16], 3.uint64) # correct = 4031760169
 echo "-abcdef- checksum; should be 1448095018 = "
 echo fletcher([25185.uint16, 25699.uint16, 26213.uint16], 3.uint64) # correct = 1448095018
+echo ""
 
+echo "FLETCHER64"
+echo "-abcde- checksum; should be 14467467625952928454 = "
+echo fletcher([1684234849.uint32, 101.uint32], 2.uint64) # correct = 14467467625952928454
+echo "-abcdef- checksum; should be 14467579776138987718 = "
+echo fletcher([1684234849.uint32, 26213.uint32], 2.uint64) # correct = 14467579776138987718
+echo ""
