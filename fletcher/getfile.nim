39f59560b2d25065367cee4624f1c851758d0660
Akito <akito.kitsune@protonmail.com>
2019-08-28 22:12:08 +0200

    Improve getfile.nim

--- fletcher/getfile.nim
+++ fletcher/getfile.nim
@@ -7,14 +7,14 @@ type
   ByteMode* = enum
     i8, i16, i32, i64
 var
-  fmtData: seq[uint8]
   size*: uint64 = 0
 
-proc getFile*(filename: string, mode: ByteMode): seq[uint8] =
+proc getFile*(filename: string, mode: ByteMode): openArray[uint8 | uint16 | uint32] =
   case mode
   of i8:
     "8-bit variation not implemented.".quit
   of i16:
+    var fmtData: seq[uint8]
     let filestream = openFileStream(filename)
     while not filestream.atEnd:
       fmtData.add(filestream.readUint8)
@@ -22,6 +22,10 @@ proc getFile*(filename: string, mode: ByteMode): seq[uint8] =
       size.inc
     return fmtData
   of i32:
+    var fmtData: seq[uint16]
+    let filestream = openFileStream(filename)
     discard
   of i64:
+    var fmtData: seq[uint32]
+    let filestream = openFileStream(filename)
     discard
